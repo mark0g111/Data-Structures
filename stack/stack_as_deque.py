ded02a4249e7ca8aa242aa891ef15c5702eed210
mark0g111 <9154547225@mail.ru>
2023-01-26 08:10:37 +0300

    stack as deque

---
+++ stack/stack_as_deque.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+class MyDequeStack:
+    def __init__(self):
+        self.array = deque()
+
+    def push(self, element):
+        self.array.append(element)
+
+    def pop(self):
+        element = self.array.pop()
+        return element
+
+    def peek(self, index):
+        return self.array[index]
+
+    def __str__(self):
+        return str(self.array)
+
+    def __iter__(self):
+        self.index = len(self.array) - 1
+        return self
+
+    def __next__(self):
+        if self.index < 0:
+            raise StopIteration()
+        result = self.array[self.index]
+        self.index -= 1
+        return result
