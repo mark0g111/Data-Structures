fc58232991c164af6c0c0334668419e5b1a3abd2
mark0g111 <9154547225@mail.ru>
2023-01-26 08:42:04 +0300

    add empty() and size()

---
+++ stack/stack_as_LifoQueue.py
@@ -0,0 +1,36 @@
+from queue import LifoQueue
+
+
+class MyQueueStack:
+    def __init__(self, length):
+        self.array = LifoQueue(maxsize=length)
+
+    def push(self, element):
+        self.array.put(element)
+
+    def pop(self):
+        element = self.array.get()
+        return element
+
+    def peek(self, index):
+        pass
+
+    def empty(self):
+        if self.array.qsize() == 0:
+            return True
+        return False
+
+    def size(self):
+        return self.array.qsize()
+
+    def __str__(self):
+        return str(self.array)
+
+
+s = MyQueueStack(10)
+s.push(2)
+s.push(3)
+s.push(4)
+print(s.size())
+print(s.empty())
+print(s)
